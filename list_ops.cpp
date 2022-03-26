01a212db135b4499bd06c27ccf50332631f4d635
Sabyasachi123618 <57725708+Sabyasachi123618@users.noreply.github.com>
2022-03-26 21:40:30 +0530

    Update list_ops.cpp

--- list_ops.cpp
+++ list_ops.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-void display(list<int> list1)
+void display(list<int> list1)   //printing the elements through iterator
 {
 	list<int>::iterator itr;
 	for(itr=list1.begin();itr!=list1.end();itr++)
@@ -19,17 +19,17 @@ int main()
 		list1.push_back(x);
 	}
 	display(list1);
-	list1.push_front(100);
+	list1.push_front(100);//push into front
 	display(list1);
-	list1.pop_back();
+	list1.pop_back();//pop from back
 	display(list1);
-	list1.pop_front();
+	list1.pop_front();//pop from front
 	display(list1);
-	list1.sort();
+	list1.sort();//sorting the list
 	display(list1);
-	list1.reverse();
+	list1.reverse();//reversing the list
 	display(list1);
 	list<int> list2=list1;
-	list1.merge(list2);
+	list1.merge(list2);//merging the list
 	display(list1);
 }
