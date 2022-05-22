e4ee058f952ed0964a453a4419820c0b79c57913
Sabyasachi <sabyasachibanerjee415@gmail.com>
2022-05-22 11:43:36 +0530

    Triangle and Square

---
+++ triangle_and_square.cpp
@@ -0,0 +1,13 @@
+ int do_calc(int x)
+    {
+        int k=(x-4)/2+1;
+        return k*(k+1)/2;
+    }
+    int countSquare(int B)
+    {
+	    // code here 
+	    if(B%2==0)
+	    do_calc(B);
+	    else
+	    do_calc(B-1);
+    }
